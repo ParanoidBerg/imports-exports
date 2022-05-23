4fc09100bccd9abcf59e517a7951b8639be2833f
Paranoid <magomaevd99@gmail.com>
2022-05-23 12:30:53 +0300

    task completed

---
+++ first.js
@@ -0,0 +1,9 @@
+export function sum (a, b) {
+    return a + b
+}
+
+export const greetings = "Lok'tar"
+export const okay = "zug-zug"
+
+
+
---
+++ second.js
@@ -0,0 +1,3 @@
+import {sum, greetings, okay} from "./first.js"
+
+console.log(sum(greetings, okay))
